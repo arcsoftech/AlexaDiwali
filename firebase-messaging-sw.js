80ed84dd23b74adb78eef843eca7f59a2b77ff48
Arihant Chhajed <arihant.chhajed@tcs.com>
2018-11-06 14:04:30 +0530

    ddasdas

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -30,45 +30,12 @@ const messaging = firebase.messaging();
 // });
 
 //Action to be performed when notification is clicked
-self.addEventListener('notificationclick', function (event) {
-  console.log("mydasdasldhasldhkl", event)
-  var url = 'http://127.0.0.1:8887';
-
-  event.notification.close(); //Close the notification
-
-  // Open the app and navigate to latest.html after clicking the notification
-  event.waitUntil(
-    clients.openWindow(url)
-  );
-
-});
 
 
 //Push notification irrespective of page is open or not
 self.addEventListener('push', function (event) {
-  console.log(`event`);
-  console.log(event);
   if (event.data) {
     console.log('This push event has data: ', event.data.text());
-    console.log(event);
-
-    var title = event.data.json().notification.title
-
-    var body = {
-      'body': event.data.json().notification.body,
-      'tag': 'pwa',
-      'icon': event.data.json().notification.icon
-    };
-    // var html='<a href="javascript:void(0);" class="dropdown-item notify-item">';
-    // html+= '<p class="notify-details">'+ title;
-    // html+='<small>'+body+ '</small></p>';
-    // document.getElementById("notification-alert").innerHTML = html;
-    event.waitUntil(
-      self.registration.showNotification(title, body)
-    );
-  } else {
-    console.log('This push event has no data.');
-  }
-
+    console.log(event);=
 
 });
